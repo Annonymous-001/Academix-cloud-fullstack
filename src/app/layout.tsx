4af8383207917b364896f20544a5d90fc643eb1e
anonymous <nitinswami12577@gmail.com>
2025-03-02 18:13:39 +0530

    fixed logo issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Academix Cloud",
   description: "Next.js School Management System",
-  // icons:{
-  //   icon:"/logo.png"
-  // }
+  icons:{
+    icon:"./favicon.ico"
+  }
 };
 
 export default function RootLayout({
