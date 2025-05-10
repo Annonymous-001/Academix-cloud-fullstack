4cb3337cc9bc7f035460c40c37cb67e457964106
anonymous <nitinswami12577@gmail.com>
2025-05-10 17:11:37 +0530

    fix some issues

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,20 @@ const nextConfig = {
       }
     ],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
+  },
+  experimental: {
+    esmExternals: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
 };
 
 export default nextConfig;
