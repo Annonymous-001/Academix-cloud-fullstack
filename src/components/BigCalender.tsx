c7a32a0691fb4d38bcd503844fd4077fe52e853e
anonymous <nitinswami12577@gmail.com>
2025-05-12 16:30:08 +0530

    fix type error

--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { Calendar, momentLocalizer, Views, View } from "react-big-calendar";
+import { Calendar, momentLocalizer, Views, View, CalendarProps } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
 
 const localizer = momentLocalizer(moment); // Ensure localizer is defined
 
+interface Event {
+  title: string;
+  start: Date;
+  end: Date;
+}
+
 const BigCalendar = ({
   data,
 }: {
-  data: { title: string; start: Date; end: Date }[];
+  data: Event[];
 }) => {
   const [view, setView] = useState<View>(Views.WORK_WEEK);
 
@@ -20,18 +26,19 @@ const BigCalendar = ({
 
   return (
     localizer && ( // ✅ Ensuring localizer is not undefined
-      <Calendar
-        localizer={localizer}
-        events={data}
-        startAccessor="start"
-        endAccessor="end"
-        views={{ week: true, day: true, work_week: true }}
-        view={view}
-        style={{ height: "98%" }}
-        onView={handleOnChangeView}
-        min={new Date(2025, 1, 0, 8, 0, 0)}
-        max={new Date(2025, 1, 0, 17, 0, 0)}
-      />
+      <div style={{ height: "98%" }}>
+        <Calendar<Event>
+          localizer={localizer}
+          events={data}
+          startAccessor="start"
+          endAccessor="end"
+          views={{ week: true, day: true, work_week: true }}
+          view={view}
+          onView={handleOnChangeView}
+          min={new Date(2025, 1, 0, 8, 0, 0)}
+          max={new Date(2025, 1, 0, 17, 0, 0)}
+        />
+      </div>
     )
   );
 };
